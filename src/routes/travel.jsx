0ada32b5e9ee394df5d338f5f5a94ffc8a966241
Carlo Sebastian La Porta <carloalaple@gmail.com>
2023-09-29 16:17:58 +0200

    fix path on compkment custom modal

--- src/routes/travel.jsx
+++ src/routes/travel.jsx
@@ -7,7 +7,7 @@ import {
   Card,
 } from 'react-bootstrap'
 import { useState } from 'react'
-import { CustomModal } from '../components/customModal'
+import { CustomModal } from '/src/components/customModal'
 function Travel() {
    const [show, setShow] = useState(false)
   const [modalData, setModalData] = useState({
